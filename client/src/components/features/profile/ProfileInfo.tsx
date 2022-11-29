c95ca84188449ad616c65ede1e7b3aec46342594
Ben Smerd <bsmerd@adam.com.au>
2022-11-29 15:56:43 +1030

    profile info and profile select updated

--- client/src/components/features/profile/ProfileInfo.tsx
+++ client/src/components/features/profile/ProfileInfo.tsx
@@ -1,1 +1,57 @@
+import car from "../../../styles/car.jpg";
+
 //! show profile info- (edit photo, bio), verify self, joined date, email address, reviews, reviews by user, cars to offer
+
+//TODO need to add in an update bio type system
+
+//TODO need a verification badge-- that shield with tick on it
+//TODO import star icon for the reviews spot
+
+const ProfileInfo = () => {
+  return (
+    <div className="flex flex-col p-3 gap-5">
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <h1 className="font-bold">Hi, I'm Ben</h1>
+          <p className="text-sm text-zinc-400">Joined in 12/2022</p>
+          <p className="font-bold text-sm underline">Edit profile</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src={car} className="w-16 h-16 rounded-xl" />
+          <p className="font-bold text-sm underline">Update photo</p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-3 border-b-2 pb-2">
+        <h1 className="font-bold">Bio</h1>
+        <p className="text-sm">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
+          itaque architecto necessitatibus cumque modi error quas, atque
+          molestias assumenda magnam?
+        </p>
+        <h1 className="font-bold">Location</h1>
+        <p className="text-sm">Adelaide, South Australia</p>
+        <h1 className="font-bold">Languages I Speak</h1>
+        <p className="text-sm">English, Danish, Spanish, French</p>
+      </div>
+      <div className="flex flex-col gap-2 border-b-2 pb-2">
+        <h1 className="font-bold">Identity Verification</h1>
+        <p>
+          Show others you're really you with the identity verification badge.
+        </p>
+        <button
+          type="submit"
+          className="border-2 rounded-xl w-[150px] mt-1 p-1 bg-indigo-200 hover:bg-indigo-300 active:bg-indigo-400 transition-all"
+        >
+          Get Verified!
+        </button>
+      </div>
+      <div className="flex flex-col gap-5">
+        <h1 className="font-bold">Reviews</h1>
+        <p className="underline">Star 0 reviews</p>
+        <p className="underline">0 reviews by you</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileInfo;
--- client/src/pages/ProfileSelect.tsx
+++ client/src/pages/ProfileSelect.tsx
@@ -1,3 +1,6 @@
+import car from "../styles/car.jpg";
+import ProfileInfo from "../components/features/profile/ProfileInfo";
+
 // TODO this is the main profile page to then select what you want to do with profile--- like airbnb
 
 //? this will be a page of its own but add a link to it
@@ -7,13 +10,29 @@
 
 //! logout button
 
-//TODO import react router for path links
+//TODO import react router for path links to the components
+
+//TODO make each list item group hover and group active click to the link
 
 const ProfileSelect = () => {
   return (
-    <div>
+    <div className="w-full">
       <h1 className="font-bold text-2xl text-center mt-2">Profile</h1>
-      <div className="flex flex-col gap-2"></div>
+      <ProfileInfo />
+      {/* <ul className="flex flex-col gap-2 p-2 w-full">
+        <li className="h-[80px] p-2 flex items-center gap-3 group hover:bg-zinc-200 hover:cursor-pointer border-b-2">
+          <ProfileInfo />
+
+          <img src={car} className="w-16 h-16 rounded-2xl" />
+          <div className="flex justify-between w-full">
+            <div className="flex flex-col">
+              <h1>Ben</h1>
+              <p className="text-xs text-zinc-400">Show profile</p>
+            </div>
+            <p className="text-2xl">{">"}</p>
+          </div>
+        </li>
+      </ul> */}
     </div>
   );
 };
