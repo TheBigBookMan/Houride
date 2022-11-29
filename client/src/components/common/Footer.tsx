50bba5985626619b1949e53703250ea3517d1ea4
Ben Smerd <bsmerd@adam.com.au>
2022-11-29 13:33:20 +1030

    added in footer

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <h1 className="text-xs text-center p-2">Houride.com.au</h1>
+    </div>
+  );
+};
+
+export default Footer;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,14 +2,16 @@ import Events from "../components/features/home/Events";
 import Listings from "../components/features/home/Listings";
 import Filter from "../components/features/home/Filter";
 import Extras from "../components/features/home/Extras";
+import Footer from "../components/common/Footer";
 
 const Home = () => {
   return (
-    <div className="flex flex-col w-full gap-2">
+    <div className="flex flex-col w-full gap-2 relative">
       <Events />
       <Filter />
       <Listings />
       <Extras />
+      <Footer />
     </div>
   );
 };
