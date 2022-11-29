db32a6291a0e1c47df7361dcf9d4e8ecb98bce5c
Ben Smerd <bsmerd@adam.com.au>
2022-11-29 12:10:33 +1030

    added in extras for the homepage

--- client/src/components/features/home/Extras.tsx
+++ client/src/components/features/home/Extras.tsx
@@ -1,9 +1,26 @@
 const Extras = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col p-2">
       <div>
-        <h1 className="font-bold text-2xl">Each Driver is verified</h1>
-        <p>Each member h</p>
+        <h1 className="font-bold text-2xl text-center">
+          Each Driver is verified
+        </h1>
+        <p>
+          Each member has been professionally verified by Houride, ensuring
+          trust and safety for all parties. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Repellat laudantium sit est, odio
+          ratione dolorum vel eius eum delectus aspernatur!
+        </p>
+      </div>
+      <div>
+        <h1 className="font-bold text-2xl text-center">Global Drivers</h1>
+        <p>
+          Wherever you may be or want to go we have drivers there, so you can
+          create an account with us and access it anywhere in the world for
+          transportation needs. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Assumenda dolorum, natus consectetur repellendus
+          neque ex reprehenderit necessitatibus ratione odio aliquid.
+        </p>
       </div>
     </div>
   );
