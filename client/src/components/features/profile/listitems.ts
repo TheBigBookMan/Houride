549e777c8e86a0732afe3aeef6fdfb6a9dff2613
Ben Smerd <bsmerd@adam.com.au>
2022-11-29 16:13:35 +1030

    updated profile list select section

---
+++ client/src/components/features/profile/listitems.ts
@@ -0,0 +1,27 @@
+export const listItems = [
+  {
+    icon: "icon",
+    title: "Personal info",
+    bio: "",
+  },
+  {
+    icon: "icon",
+    title: "Settings",
+    bio: "",
+  },
+  {
+    icon: "icon",
+    title: "Car Rental",
+    bio: "Lend out a car for hire",
+  },
+  {
+    icon: "icon",
+    title: "Support",
+    bio: "",
+  },
+  {
+    icon: "icon",
+    title: "Change Details",
+    bio: "Language, currency and location",
+  },
+];
--- client/src/pages/ProfileSelect.tsx
+++ client/src/pages/ProfileSelect.tsx
@@ -1,5 +1,6 @@
 import car from "../styles/car.jpg";
 import ProfileInfo from "../components/features/profile/ProfileInfo";
+import { listItems } from "../components/features/profile/listitems";
 
 // TODO this is the main profile page to then select what you want to do with profile--- like airbnb
 
@@ -14,14 +15,15 @@ import ProfileInfo from "../components/features/profile/ProfileInfo";
 
 //TODO make each list item group hover and group active click to the link
 
+//TODO import icons for each section
+
 const ProfileSelect = () => {
   return (
     <div className="w-full">
       <h1 className="font-bold text-2xl text-center mt-2">Profile</h1>
-      <ProfileInfo />
-      {/* <ul className="flex flex-col gap-2 p-2 w-full">
-        <li className="h-[80px] p-2 flex items-center gap-3 group hover:bg-zinc-200 hover:cursor-pointer border-b-2">
-          <ProfileInfo />
+      <ul className="flex flex-col gap-5 p-2 w-full">
+        <li className="h-[80px] p-2 flex items-center gap-3 group hover:bg-zinc-200 hover:cursor-pointer rounded-2xl transition-all border-b-2">
+          {/* <ProfileInfo /> */}
 
           <img src={car} className="w-16 h-16 rounded-2xl" />
           <div className="flex justify-between w-full">
@@ -32,7 +34,19 @@ const ProfileSelect = () => {
             <p className="text-2xl">{">"}</p>
           </div>
         </li>
-      </ul> */}
+        {listItems.map((item) => (
+          <li className="h-[50px] flex justify-between items-center group hover:bg-zinc-200 hover:cursor-pointer rounded-2xl transition-all  p-2">
+            <div className="flex gap-2 items-center">
+              <p>{item.icon}</p>
+              <div className="flex flex-col">
+                <p>{item.title}</p>
+                <p className="text-sm text-zinc-400">{item.bio}</p>
+              </div>
+            </div>
+            <p className="text-2xl">{">"}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
