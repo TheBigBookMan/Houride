a1fc4c21b71bd14619a9ef28c12f6b9ed38396f7
Ben Smerd <bsmerd@adam.com.au>
2022-11-29 12:10:21 +1030

    added in fake hardcode data for styling of listing

--- client/src/components/features/home/Listings.tsx
+++ client/src/components/features/home/Listings.tsx
@@ -1,70 +1,26 @@
 import car from "../../../styles/car.jpg";
 import { AiFillStar } from "react-icons/ai";
+import { fakeListingData } from "../../../utils/fakeListingData";
 
 const Listings = () => {
   return (
     <div>
-      <ul className="flex flex-wrap p-2 gap-5 h-[600px] overflow-y-scroll">
-        <li className="flex flex-col">
-          <img className="w-40 rounded-lg" src={car} />
-          <div className="flex flex-row justify-between">
-            <h1 className="font-bold">Aston Martin</h1>
-            <p className="flex items-center">
-              5<AiFillStar />
-            </p>
-          </div>
-          <p className="text-zinc-400 text-sm">Weddings/Formals</p>
-          <p>User: BennyBoy</p>
-          <p>$1200 p/h</p>
-        </li>
-        <li className="flex flex-col">
-          <img className="w-40 rounded-lg" src={car} />
-          <div className="flex flex-row justify-between">
-            <h1 className="font-bold">Aston Martin</h1>
-            <p className="flex items-center">
-              5<AiFillStar />
-            </p>
-          </div>
-          <p className="text-zinc-400 text-sm">Weddings/Formals</p>
-          <p>User: BennyBoy</p>
-          <p>$1200 p/h</p>
-        </li>
-        <li className="flex flex-col">
-          <img className="w-40 rounded-lg" src={car} />
-          <div className="flex flex-row justify-between">
-            <h1 className="font-bold">Aston Martin</h1>
-            <p className="flex items-center">
-              5<AiFillStar />
-            </p>
-          </div>
-          <p className="text-zinc-400 text-sm">Weddings/Formals</p>
-          <p>User: BennyBoy</p>
-          <p>$1200 p/h</p>
-        </li>
-        <li className="flex flex-col">
-          <img className="w-40 rounded-lg" src={car} />
-          <div className="flex flex-row justify-between">
-            <h1 className="font-bold">Aston Martin</h1>
-            <p className="flex items-center">
-              5<AiFillStar />
-            </p>
-          </div>
-          <p className="text-zinc-400 text-sm">Weddings/Formals</p>
-          <p>User: BennyBoy</p>
-          <p>$1200 p/h</p>
-        </li>
-        <li className="flex flex-col">
-          <img className="w-40 rounded-lg" src={car} />
-          <div className="flex flex-row justify-between">
-            <h1 className="font-bold">Aston Martin</h1>
-            <p className="flex items-center">
-              5<AiFillStar />
-            </p>
-          </div>
-          <p className="text-zinc-400 text-sm">Weddings/Formals</p>
-          <p>User: BennyBoy</p>
-          <p>$1200 p/h</p>
-        </li>
+      <ul className="flex flex-wrap p-2 gap-5 h-[600px] overflow-y-scroll justify-center">
+        {fakeListingData.map((data) => (
+          <li className="flex flex-col">
+            <img className="w-72 rounded-2xl" src={car} />
+            <div className="flex flex-row justify-between">
+              <h1 className="font-bold">{data.car}</h1>
+              <p className="flex items-center">
+                {data.rating}
+                <AiFillStar />
+              </p>
+            </div>
+            <p className="text-zinc-400 text-sm">{data.event}</p>
+            <p className="text-sm">User: {data.user}</p>
+            <p className="font-bold">${data.price} p/h</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
