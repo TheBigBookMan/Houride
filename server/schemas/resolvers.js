399b77c15074f3890acb0bd098933c5b93b4e12c
Ben Smerd <bsmerd@adam.com.au>
2022-11-30 20:01:58 +1030

    setup the graphql server schemas, resolvers and typedefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,7 @@
+const { AuthenticationError } = require("apollo-server-express");
+// TODO import models
+const { signToken, setCookie } = require("../utils/auth");
+
+const resolvers = {};
+
+module.exports = resolvers;
