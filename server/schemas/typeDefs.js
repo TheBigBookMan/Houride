74fdcfa76510674b22f1a972cd6e282c2ba74e1d
Ben Smerd <bsmerd@adam.com.au>
2022-11-30 22:32:01 +1030

    create query type in typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,18 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Query {
+    car(carId: ID!): Car
+    cars: [Car]
+    customer(customerId: ID!): Customer
+    customers: [Customer]
+    event(eventId: ID!): Event
+    events: [Event]
+    transaction(transactionId: ID!): Transaction
+    transactions: [Transaction]
+    user(userId: ID!): User
+    users: [User]
+  }
+`;
 
 module.exports = typeDefs;
