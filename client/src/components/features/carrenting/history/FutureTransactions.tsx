0343727333a396bb02a0259a03c324692c250866
Ben Smerd <bsmerd@adam.com.au>
2022-12-15 07:17:42 +1030

    created transactions components

---
+++ client/src/components/features/carrenting/history/FutureTransactions.tsx
@@ -0,0 +1,47 @@
+const bookingcode = [
+  {
+    name: "Ben Smerd",
+    event: "Formal",
+    location: "Adelaide",
+    date: "22/02/2022",
+    time: "11:05AM",
+  },
+  {
+    name: "Ben Smerd",
+    event: "Formal",
+    location: "Adelaide",
+    date: "22/02/2022",
+    time: "11:05AM",
+  },
+  {
+    name: "Ben Smerd",
+    event: "Formal",
+    location: "Adelaide",
+    date: "22/02/2022",
+    time: "11:05AM",
+  },
+  {
+    name: "Ben Smerd",
+    event: "Formal",
+    location: "Adelaide",
+    date: "22/02/2022",
+    time: "11:05AM",
+  },
+  {
+    name: "Ben Smerd",
+    event: "Formal",
+    location: "Adelaide",
+    date: "22/02/2022",
+    time: "11:05AM",
+  },
+];
+
+const FutureTransactions = () => {
+  return (
+    <div>
+      <h1>Future</h1>
+    </div>
+  );
+};
+
+export default FutureTransactions;
---
+++ client/src/components/features/carrenting/history/PastTransactions.tsx
@@ -0,0 +1,9 @@
+const PastTransactions = () => {
+  return (
+    <div>
+      <h1>Past</h1>
+    </div>
+  );
+};
+
+export default PastTransactions;
