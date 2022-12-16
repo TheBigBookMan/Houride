0a7301f444baccb2e6ffeed959d930827070c9ef
Ben Smerd <bsmerd@adam.com.au>
2022-12-17 09:47:23 +1030

    added changeDetails component and styling

--- client/src/components/features/profile/ChangeDetails.tsx
+++ client/src/components/features/profile/ChangeDetails.tsx
@@ -1,9 +1,37 @@
 //! Change language/change currency
+const detailsChange = [
+  {
+    title: "Change language",
+    subTitle: "Select the language to be used",
+  },
+  {
+    title: "Change currency",
+    subTitle: "Choose which currency to use",
+  },
+  {
+    title: "Change location",
+    subTitle: "Select where you would like to be located",
+  },
+];
 
 const ChangeDetails = () => {
   return (
-    <div>
-      <h1>Change details</h1>
+    <div className="flex flex-col p-5 gap-5">
+      <h1 className="font-bold text-lg">Account settings</h1>
+      <ul className="flex flex-col">
+        {detailsChange.map((item) => (
+          <li
+            key={item.title}
+            className="flex items-center justify-between border-b-2 rounded-lg py-5 group hover:cursor-pointer hover:bg-indigo-100 rounded-lg p-2 pr-4"
+          >
+            <div className="flex flex-col">
+              <h1>{item.title}</h1>
+              <p className="text-sm text-zinc-400">{item.subTitle}</p>
+            </div>
+            <p className="font-bold underline ">Edit</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
