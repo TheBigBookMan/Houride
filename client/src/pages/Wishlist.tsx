77e1086f57ecc064c2fb22194334638f3fbd30e1
Ben Smerd <bsmerd@adam.com.au>
2022-12-17 09:32:23 +1030

    add react icons to wishlist page

--- client/src/pages/Wishlist.tsx
+++ client/src/pages/Wishlist.tsx
@@ -1,4 +1,5 @@
 import car from "../styles/car.jpg";
+import { AiFillStar, AiFillHeart } from "react-icons/ai";
 
 const hardcode = [
   {
@@ -41,28 +42,28 @@ const hardcode = [
 
 //TODO this is a favourites page for the user to save cars
 
-//TODO import react icons star for faourites
-
-//TODO rating, event, user, price
+//TODO mutations for removing from favourites
 
 const Wishlist = () => {
   return (
     <div>
       <h1 className="font-bold text-2xl text-center mt-2">Wishlist</h1>
       <div className="h-full w-full">
-        <ul className="flex flex-col">
+        <ul className="flex flex-col gap-2 p-2">
           {hardcode.map((info) => (
-            <li className="border-solid border-2 border-black flex h-[100px]">
+            <li className="border-b border-b-2 flex h-[100px]">
               <div className="flex flex-col">
                 <img src={car} className="w-28" />
-                <div className="flex items-center">
+                <div className="flex items-center gap-2">
                   <h1 className="text-sm font-bold">{info.user}</h1>
-                  <p>ST</p>
+                  <AiFillHeart className="cursor-pointer" />
                 </div>
               </div>
               <div className="flex w-full justify-around">
                 <p>{info.event}</p>
-                <p>{info.rating} ST</p>
+                <p className="flex justify-center">
+                  {info.rating} <AiFillStar />
+                </p>
                 <p>${info.price} p/h</p>
               </div>
             </li>
