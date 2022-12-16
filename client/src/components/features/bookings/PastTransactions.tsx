f71d5c13ddd4dd9b271bb4ef5082334b2ea1fd5d
Ben Smerd <bsmerd@adam.com.au>
2022-12-17 10:05:53 +1030

    created bookings component and past and future transaction components

---
+++ client/src/components/features/bookings/PastTransactions.tsx
@@ -0,0 +1,62 @@
+const bookingcode = [
+  {
+    name: "ANDY kyrakousasd",
+    event: "Formal",
+    location: "Adelaide",
+    date: "22/02/2022",
+    time: "11:05AM",
+  },
+  {
+    name: "ANDY kyrakousasd",
+    event: "wedding",
+    location: "port elliot",
+    date: "23/22/2021",
+    time: "11:05AM",
+  },
+  {
+    name: "ANDY kyrakousasd",
+    event: "Cemetary",
+    location: "Melbs",
+    date: "11/02/1022",
+    time: "11:05AM",
+  },
+  {
+    name: "ANDY kyrakousasd",
+    event: "Formal",
+    location: "Adelaide",
+    date: "22/02/2022",
+    time: "11:05AM",
+  },
+  {
+    name: "ANDY kyrakousasd",
+    event: "Formal",
+    location: "Adelaide",
+    date: "22/02/2022",
+    time: "11:05AM",
+  },
+];
+
+const PastTransactions = () => {
+  return (
+    <div>
+      <ul className="flex flex-col gap-2 h-full w-full p-2">
+        {bookingcode.map((booking) => (
+          <li className="border-b border-b-2 h-[80px] flex justify-between hover:cursor-pointer hover:bg-indigo-100 rounded-lg p-1">
+            <div className="flex flex-col">
+              <h1 className="font-bold">{booking.name}</h1>
+              <p>Event- {booking.event}</p>
+              <p>Location- {booking.location}</p>
+            </div>
+            <div className="flex flex-col">
+              <p>Date- {booking.date}</p>
+              <p>Time- {booking.time}</p>
+              <p className="underline hover:cursor-pointer">view transaction</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PastTransactions;
--- client/src/pages/Bookings.tsx
+++ client/src/pages/Bookings.tsx
@@ -1,9 +1,35 @@
-//TODO this is a transaction page with upcoming bookings and previous transactions.bookings
+import FutureTransactions from "../components/features/bookings/FutureTransactions";
+import PastTransactions from "../components/features/bookings/PastTransactions";
+import { useState } from "react";
+
+//TODO this is a transaction page for customers with upcoming bookings and previous transactions.bookings
 
 const Bookings = () => {
+  const [isFuture, setIsFuture] = useState<boolean>(true);
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full flex flex-col gap-2">
       <h1 className="font-bold text-2xl text-center mt-2">Bookings</h1>
+      <div className="flex flex-col items-center justify-between">
+        <div className="flex gap-2">
+          <button
+            onClick={(): void => setIsFuture(true)}
+            className={`${
+              isFuture ? "bg-indigo-300" : "bg-zinc-100"
+            } text-sm border-2 rounded-xl p-2 w-[150px]  hover:bg-indigo-300 active:bg-indigo-400 transition-all`}
+          >
+            Future transactions
+          </button>
+          <button
+            onClick={(): void => setIsFuture(false)}
+            className={`${
+              !isFuture ? "bg-indigo-300" : "bg-zinc-100"
+            } text-sm border-2 rounded-xl p-2 w-[150px] hover:bg-indigo-300 active:bg-indigo-400 transition-all`}
+          >
+            Past transactions
+          </button>
+        </div>
+      </div>
+      {isFuture ? <FutureTransactions /> : <PastTransactions />}
     </div>
   );
 };
